b5dc978630608ed909c7c4f601647f5d1643860b
Ryuichi Ueda <ryuichiueda@gmail.com>
2024-03-05 10:08:45 +0900

    Refactor

--- src/elements/word/tilde_expansion.rs
+++ src/elements/word/tilde_expansion.rs
@@ -5,21 +5,26 @@ use crate::ShellCore;
 use crate::elements::word::Word;
 
 pub fn eval(word: &mut Word, core: &mut ShellCore) {
-    if word.subwords.len() == 0
-    || word.subwords[0].get_text() != "~" {
-        return;
-    }
+    let length = match prefix_length(word) {
+        0 => return,
+        n => n,
+    };
 
-    let mut text = String::new();
-    let mut pos = 1;
-    for sw in &word.subwords[1..] {
-        if sw.get_text() == "/" {
-            break;
-        }
-        text += &sw.get_text();
-        pos += 1;
-    }
+    let text: String = word.subwords[1..length].iter()
+               .map(|e| e.get_text().to_string())
+               .collect::<Vec<String>>()
+               .concat();
 
-    eprintln!("{}, {}", text, pos);
+    eprintln!("PREFIX: {}", text);
 }
 
+fn prefix_length(word: &Word) -> usize {
+    if word.subwords.len() == 0 || word.subwords[0].get_text() != "~" {
+        return 0;
+    }
+
+    match word.subwords.iter().position(|e| e.get_text() == "/") {
+        None    => word.subwords.len(),
+        Some(n) => n,
+    }
+}

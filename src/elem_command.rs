d931deeb46ffdca444458bbc7a45c4004261872f
Masao Yano <masao.yano@gmail.com>
2024-09-27 16:34:31 +0900

    入力値をパースして配列に変換

--- src/elem_command.rs
+++ src/elem_command.rs
@@ -1,23 +1,29 @@
 //SPDX-FileCopyrightText: 2022 Ryuichi Ueda ryuichiueda@gmail.com
 //SPDX-License-Identifier: BSD-3-Clause
 
+use crate::{Feeder, ShellCore};
 use std::process;
-use crate::{ShellCore,Feeder};
 
 pub struct Command {
     pub text: String,
 }
 
 impl Command {
-    pub fn exec(&mut self, _core: &mut ShellCore) { //引数_coreはまだ使いません
+    pub fn exec(&mut self, _core: &mut ShellCore) {
         if self.text == "exit\n" {
             process::exit(0);
         }
-        print!("{}", self.text);
+
+        let mut words = vec![];
+        for w in self.text.trim_end().split(' ') {
+            words.push(w);
+        }
+
+        println!("{:?}", words);
     }
 
     pub fn parse(feeder: &mut Feeder, _core: &mut ShellCore) -> Option<Command> {
         let line = feeder.consume(feeder.remaining.len());
-        Some( Command {text: line} )
+        Some(Command { text: line })
     }
 }

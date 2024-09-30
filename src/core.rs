05720343041bd01ee4be46f5a12909000a99fddc
Masao Yano <masao.yano@gmail.com>
2024-09-30 12:41:31 +0900

    wait_process()実装

--- src/core.rs
+++ src/core.rs
@@ -1,7 +1,10 @@
 //SPDX-FileCopyrightText: 2022 Ryuichi Ueda ryuichiueda@gmail.com
 //SPDX-License-Identifier: BSD-3-Clause
 
-use nix::unistd::Pid;
+use nix::{
+    sys::wait::{self, WaitStatus},
+    unistd::Pid,
+};
 
 pub struct ShellCore {
     pub history: Vec<String>,
@@ -16,5 +19,18 @@ impl ShellCore {
         conf
     }
 
-    pub fn wait_process(&mut self, child: Pid) {}
+    pub fn wait_process(&mut self, child: Pid) {
+        let exit_status = match wait::waitpid(child, None) {
+            Ok(WaitStatus::Exited(_pid, status)) => status,
+            Ok(unsupported) => {
+                eprintln!("Unsupported: {:?}", unsupported);
+                1
+            }
+            Err(err) => {
+                eprintln!("Error: {:?}", err);
+                1
+            }
+        };
+        eprintln!("終了ステータス： {}", exit_status);
+    }
 }
